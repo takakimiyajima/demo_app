a589d890ad504f31a0dea79f8756a050ab4d96ad
宮嶋 <takaki.mz@icloud.com>
2016-01-25 11:59:04 +0000

    midstream1

---
+++ device_sample/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviceSample::Application.config.secret_key_base = 'be63040a8a5ac71d1ad27321b39a1b18b72c36b6f28e394a398a7171d211ab69305cc4a2253e4a70b9a7c9a41856ae7a6664aa246b05ba215d12a55c06916eb0'
